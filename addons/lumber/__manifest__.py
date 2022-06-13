e332151f7eb4ffa25e572c9827e9abe3fbfe3e54
Ben Stannard <ben.stannard@gmail.com>
2022-06-13 05:52:21 -0700

    added scoffolding for addon and poetry for dependancy mgmt

---
+++ addons/lumber/__manifest__.py
@@ -0,0 +1,19 @@
+{
+        'name': 'Lumber',
+        'version': '0.1',
+        'license': 'AGPL-3',
+        'depends': ['base'],
+        'author': 'Ben Stannard',
+        'website': 'https://github.com/benstannard',
+        'category': 'Services',
+        'summary': 'Guide for lumber sales',
+        'description': """
+        Test module using lumber to learn about Odoo architecture
+        """,
+        'data': [
+            # 'views/lumber_view.xml',
+            ],
+        'application': True,
+        'installable': True,
+        'auto_install': False,
+}
---
+++ addons/lumber/models/lumber.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class LumberPackage(models.Model):
+    _name = 'lumber.package'
+    _description = 'A lumber package purchase'
+
+    date = fields.Date(required=True)
+    name = fields.Text(required=True)
+    kind = fields.Selection(
+            string='Type',
+            selection=[('boards', 'Boards'), ('dimensional', 'Dimensional'), ('timber', 'Timber')]
+    thickness = fields.Float(require=True)
+    width = fields.Float(require=True)
+    lengeth = fields.Float(require=True)
+    quantiy = fields.Integer(require=True)
+    price = fields.Float(require=True)
